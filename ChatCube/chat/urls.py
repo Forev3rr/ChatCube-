c1b0030d9472c174026a8f12153661d9e9b86a70
Ryan Pachal <123977p@acadiau.ca>
2016-11-26 15:49:08 -0400

    Allowing users to enter new messages in a room

--- ChatCube/chat/urls.py
+++ ChatCube/chat/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url, include
 from django.views.generic import ListView, DetailView
 from .models import Message, Room
-
+from .views import aroom
 urlpatterns = [ url(r'^$', ListView.as_view(queryset=Room.objects.all().order_by("-date")[:25],
                                             template_name="chat/rooms.html")),
-                url(r'^(?P<pk>\d+)$', DetailView.as_view(model = Room,
-                                                         template_name = 'chat/room.html'))
+                # url(r'^(?P<pk>\d+)$', DetailView.as_view(model = Room,
+                #                                          template_name = 'chat/room.html')),
+                url(r'^(?P<pk>\d+)$', aroom, name='aroom')
 ]
--- ChatCube/chat/views.py
+++ ChatCube/chat/views.py
@@ -1,6 +1,32 @@
 from rest_framework import viewsets
-from .models import Room, Message
+from .models import Room, Message, MessageForm
 from .serializers import RoomSerializer, MessageSerializer
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from django.shortcuts import render
+from django.contrib import messages
+
+
+def aroom(request, pk):
+    room = Room.objects.get(pk=pk)
+    messages = Message.objects.filter(targets=room)
+    mess = []
+    for m in messages:
+        mess.append(m.message)
+    if request.method == 'POST':
+        # form = MessageForm(request.POST)
+        # print form
+        # if form.is_valid():
+        #     instance = form.save(commit=False)
+        # instance.targets = room
+        # instance.sender = request.user
+        message = request.POST.get('message', '')
+        message_obj = Message(message=message, sender=request.user, targets=room)
+        message_obj.save()
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+    return render(request, 'chat/room.html', {'content':[room.name, mess]})
 
 class RoomViewSet(viewsets.ModelViewSet):
     model = Room
