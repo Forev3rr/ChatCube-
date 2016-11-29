d33a29f2c05221f5f8bb3b2e043ffa4d13395d19
Ryan Pachal <123977p@acadiau.ca>
2016-11-28 21:46:44 -0400

    Adding an add room button for admins to use

--- ChatCube/chat/urls.py
+++ ChatCube/chat/urls.py
@@ -2,8 +2,14 @@ from django.conf.urls import url, include
 from django.views.generic import ListView, DetailView
 from .models import Message, Room
 from .views import aroom
-urlpatterns = [ url(r'^$', ListView.as_view(queryset=Room.objects.all().order_by("-date")[:25],
-                                            template_name="chat/rooms.html")),
+
+class RoomView(ListView):
+    model = Room
+    template_name = "chat/rooms.html"
+    def get_queryset(self):
+        return Room.objects.filter(participants=self.request.user)
+
+urlpatterns = [ url(r'^$', RoomView.as_view(), name='rooms'),
                 # url(r'^(?P<pk>\d+)$', DetailView.as_view(model = Room,
                 #                                          template_name = 'chat/room.html')),
                 url(r'^(?P<pk>\d+)$', aroom, name='aroom')
